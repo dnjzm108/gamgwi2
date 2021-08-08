be697a4b91073dd29731a7a83f73e486aec9d6fd
algml <algml9603@gmail.com>
2021-08-08 18:03:39 +0900

    makepostlist

--- back/routers/board/index.js
+++ back/routers/board/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller')
 
-
+router.post('/list',controller.post_list)
 
 
 router.post('/modify',controller.modify_succece)
