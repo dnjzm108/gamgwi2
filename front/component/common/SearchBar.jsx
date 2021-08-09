739db9a89c8e55bf22b7f189db473f8102356890
hyejjun <hjlee9725@gmail.com>
2021-08-09 11:24:22 +0900

    searchbar 수정2

--- front/component/common/SearchBar.jsx
+++ front/component/common/SearchBar.jsx
@@ -11,13 +11,13 @@ const SearchBar = () => {
             <SearchBarWrap>
                 <form method="POST" action="http://localhost:3500/board/list">
                     <select name="search">
-                        <option value="noen" disabled>==선택==</option>
+                        <option value="none" disabled>==선택==</option>
                         <option value="writer" selected>작성자</option>
                         <option value="content">내용</option>
                         <option value="title">제목</option>
                     </select>
                     <input type="text" name="searchedValue"/>
-                    <button>검색</button>
+                    <button type="submit" onSubmit={searchSubmit}>Search</button>
                 </form>
             </SearchBarWrap>
         </>
