26ebe6f4e4bd630147292e296bce75a9912ae9db
hyejjun <hjlee9725@gmail.com>
2021-08-09 11:14:00 +0900

    to merge

--- back/routers/board/index.js
+++ back/routers/board/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller')
 
+router.post('/list',controller.post_list)
+
+router.get('/list',controller.get_list)
+router.get('/likes',controller.get_likes)
+router.get('/write',controller.get_write)
+
+
+
+
+
 
 
 
@@ -10,7 +20,7 @@ router.post('/modify',controller.modify_succece)
 
 router.get('/delete',controller.Delete)
 router.get('/modify',controller.modify)
-router.get('/list',controller.list)
+// router.get('/list',controller.list) //
 router.post('/write',controller.write)
 router.get('/reply',controller.view_reply)
 
--- front/component/common/SearchBar.jsx
+++ front/component/common/SearchBar.jsx
@@ -2,14 +2,14 @@ const SearchBar = () => {
     return (
         <>
             <div>
-                <form>
+                <form method="POST" action="http://localhost:3500/board/list">
                     <select name="search">
                         <option value="noen" disabled>==선택==</option>
                         <option value="writer" selected>작성자</option>
                         <option value="content">내용</option>
                         <option value="title">제목</option>
                     </select>
-                    <input type="text" />
+                    <input type="text" name="searchedValue"/>
                     <button>검색</button>
                 </form>
             </div>
