0e24ab16161e29898c5e25f1bb1b22bd4453f8ce
algml <algml9603@gmail.com>
2021-08-10 11:16:46 +0900

    tableConnect

--- back/models/index.js
+++ back/models/index.js
@@ -32,17 +32,13 @@ if (config.use_env_variable) {
 //     db[model.name] = model;
 //   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+
 
 db.Weather = Weather
 db.Comment = Comment
 db.User = User
 db.Board = Board
-db.Link = Like
+db.Like = Like
 db.BackgroundImg = BackgroundImg
 
 db.sequelize = sequelize;
@@ -54,5 +50,24 @@ Board.init(sequelize)
 Weather.init(sequelize)
 Like.init(sequelize)
 BackgroundImg.init(sequelize)
+
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+/*
+// 관계설정이 되지 않았던 이유===========================================
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+위의 코드가 처음에 db.Weather = Weather ... 의 코드 위에 있어서 db가 비어있는 상태로 실행되어 테이블간 관계설정이 되지 않았던 것!! 
+=> 그래서 위의 코드를 db에 테이블을 담은 다음인 그 아래에 내려주어 관계설정을 함
+*/ 
+
 
 module.exports = db;
