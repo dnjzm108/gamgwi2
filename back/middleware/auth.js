31704c4b78846a5ce0ba3a49644e193ef7950ef8
dnjzm108 <dnjzm108@kakao.com>
2021-08-13 16:08:19 +0900

    cookie_check

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -2,33 +2,39 @@ require('dotenv').config();
 const crypto = require('crypto');
 
 module.exports = (req, res, next) => {
-    let {AccessToken} = req.cookies
-    if(AccessToken == undefined){
+    let { AccessToken } = req.cookies
+    if (AccessToken == undefined) {
         console.log('로그인을 진행해주세요');
-        // res.redirect('/?msg=로그인을 진행해 주세요.');
+        res.json({ cookie:'Fail' });
         return 0;
     }
 
-    let [header,payload,sign] = AccessToken.split('.');
-    let signature = getSignature(header,payload);
+    let [header, payload, sign] = AccessToken.split('.');
+    let signature = getSignature(header, payload);
     console.log(signature);
 
     if (sign == signature) {
         console.log('검증된 토큰입니다');
-        let {userid,userIdx,userpw,exp} = JSON.parse(Buffer.from(payload,'base64').toString());
-        console.log(userid,userIdx,userpw,exp);
+        let { userid,userIdx,userpw, exp } = JSON.parse(Buffer.from(payload, 'base64').toString());
+        let user_info = {userid, userIdx};
+
         let nexp = new Date().getTime();
+
         if (nexp > exp) {
             res.clearCookie('AccessToken');
             console.log('토큰 만료');
-            // res.redirect('/?msg=토큰만료');
+            res.json({ cookie: 'Fail '});
             return 0;
         }
-        req.userpw = userpw;
-        next();
+        res.json({
+            cookie: 'success',
+            user_info
+        });
+        
     } else {
-    //    res.redirect('/?msg=부적절한 토큰')
-    console.log('부적절한 토큰');
+        res.clearCookie('AccessToken');
+        res.json({ cookie: 'Fail' });
+        console.log('부적절한 토큰');
     }
 
 }
